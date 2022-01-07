2a79f3d461237ad90797781598f983b4bfc7c7ff
AdsonNascimento <adson.nascimento.112@ufrn.edu.br>
2022-01-07 08:42:04 -0300

    verificação
    verificação de número de sala e troca dinamica ejs id room

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,21 +5,36 @@ module.exports = {
     const db = await Database()
     const pass = req.body.password
     let roomId
+    let isRoom = true
+    while(isRoom) {
+      //gera o número da sala
+      for(var i = 0; i < 6; i++){
+        i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : roomId += Math.floor(Math.random() * 10).toString()
+      }
 
-    for(var i = 0; i < 6; i++){
-      i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : roomId += Math.floor(Math.random() * 10).toString()
+      //verifica se o número da sala esta repetindo
+      const roomExistIds = await db.all(`SELECT id FROM rooms`)
+      isRoom = roomExistIds.some(roomExistIds => roomExistIds === roomId)
+
+      if(!isRoom){
+      //insere os dados no banco de dados
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomId)},
+          ${pass}
+        )`)
+      }
     }
-
-    await db.run(`INSERT INTO rooms (
-      id,
-      pass
-    ) VALUES (
-      ${parseInt(roomId)},
-      ${pass}
-    )`)
 
     await db.close()
 
     res.redirect(`/room/${roomId}`)
+  },
+
+  open(req, res){
+    const roomId = req.params.room
+    res.render("room", {roomId: roomId})
   }
 }
--- src/route.js
+++ src/route.js
@@ -8,7 +8,7 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'creat-pass'}))
 
-route.get('/room/:room', (req, res) => res.render("room"))
+route.get('/room/:room', RoomController.open)
 
 // formato que o formulário de dentro da modal passa inforamção
 route.post('/question/:room/:question/:action', QuestionController.index)
